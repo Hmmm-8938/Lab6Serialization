78eef23033e9ea5353803ac99aac586f0f4bad0b
Benjamin Mellott <Benjamin.Mellott@edu.sait.ca>
2024-03-11 14:29:58 -0600

    Changes still a work in progress

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Runtime.Serialization.Formatters.Binary;
+using System.Text.Json;
 
 namespace Lab6Serialization
 {
@@ -7,31 +8,71 @@ namespace Lab6Serialization
         static void Main(string[] args)
         {
             #pragma warning disable SYSLIB0011
-            string event_txt = $"{(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName)}\\event.txt";
-            string text_txt = $"{(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName)}\\text.txt";
-            string s1 = "";
+            string event_txt = ($"{(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName)}\\event.txt");
+            string text_txt = ($"{(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName)}\\text.txt");
+            string event_json = ($"{(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName)}\\event.json");
+            int index = 0;
 
             Event e1 = new Event()
             {
                 EventNumber = 1,
                 Location = "Calgary"
             };
+            Event j1 = new Event()
+            {
+                EventNumber = 1,
+                Location = "#4 Privet Drive"
+            };
+            Event j2 = new Event()
+            {
+                EventNumber = 2,
+                Location = "Diagon Alley"
+            };
+            Event j3 = new Event()
+            {
+                EventNumber = 3,
+                Location = "Gringotts"
+            };
+            Event j4 = new Event()
+            {
+                EventNumber = 4,
+                Location = "Hogwarts"
+            };
+
+            List<Event> eventListJSON = new List<Event>();
+            eventListJSON.Add(j1);
+            eventListJSON.Add(j2);
+            eventListJSON.Add(j3);
+            eventListJSON.Add(j4);
+
+            string s1 = "Hackathon";
 
             // Using Binary Formatter
             SerializeEvent(e1, event_txt);
-            ReadFromFile(text_txt);
-            string s2 = (DeserializeString(text_txt, s1).ToUpper());
             Event e2 = (DeserializeEvent(event_txt, e1));
+
+            foreach (Event JSON in eventListJSON)
+            {
+                Event j = new Event();
+                SerializeJSON(j, event_json, index);
+                j = (DeserializeJSON(event_json));
+                Console.WriteLine(j);
+            }
+            
+
+            WriteToFile(s1, text_txt);
+            string s2 = (ReadFromFile(text_txt).ToUpper());
+            
 
             Console.WriteLine($"\nExpected Program Output:" +
                               $"\n************************" +
                               $"\n{e2.EventNumber}" +
                               $"\n{e2.Location}" +
                               $"\nTech Competition" +
-                              $"\nIn Word: {s2}" +
-                              $"\nThe First Character is: {s2[0]}" +
-                              $"\nThe Middle Character is: {s2[4]}" +
-                              $"\nThe Last Character is: {s2[8]}");
+                              $"\nIn Word: {event_json}" +
+                              $"\nThe First Character is: {event_json}" +
+                              $"\nThe Middle Character is: {event_json}" +
+                              $"\nThe Last Character is: {event_json}");
             Console.ReadKey();
         }
 
@@ -61,26 +102,44 @@ namespace Lab6Serialization
             return e1;
         }
 
-        private static String DeserializeString(string path, string s1)
+        // Using JSON for Serialization
+        private static void SerializeJSON(Event j, string path, int index)
         {
-            BinaryFormatter binaryFormatter = new BinaryFormatter();
-            using (FileStream fs = new FileStream(path, FileMode.Open))
+            
+            string stringJSON = JsonSerializer.Serialize(j);
+            if (index == 0)
             {
-                // deserialize the txt file and cast it into a String object
-                // return type of Deserialize is Object Class.
-                s1 = (String)binaryFormatter.Deserialize(fs);
+                File.WriteAllText(path, stringJSON);
+                index += 1;
             }
-            return s1;
+            else
+            {
+                File.AppendAllText(path, stringJSON);
+            }
+            Console.WriteLine("JSON serialization done");
         }
 
-
-        public static void ReadFromFile(string path)
+        private static Event DeserializeJSON(string path)
         {
-            BinaryFormatter binaryFormatter = new BinaryFormatter();
-            using (FileStream fs = new FileStream(path, FileMode.Create))
+            Event j = JsonSerializer.Deserialize<Event>(File.ReadAllText(path));
+            Console.WriteLine("Deserializing using JSON");
+            return j;
+        }
+
+        public static string ReadFromFile(string path)
+        {
+            string line = "";
+            using (StreamReader sr = new StreamReader(path))
             {
-                // serializing the student object
-                binaryFormatter.Serialize(fs, "Hackathon");
+                return line;
+            }
+        }
+
+        public static void WriteToFile(string s1, string path)
+        {
+            using (StreamWriter sw = new StreamWriter(path))
+            {
+                sw.WriteLine(s1);
             }
         }
     }
